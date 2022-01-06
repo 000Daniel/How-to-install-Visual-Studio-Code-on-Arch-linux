fb0437982ac0b0b2844cea62c1824f35079bcd74
000Daniel <90350173+000Daniel@users.noreply.github.com>
2022-01-06 18:16:08 +0200

    Update auto-Installer-Visual-Studio-Code.sh

--- auto-Installer-Visual-Studio-Code.sh
+++ auto-Installer-Visual-Studio-Code.sh
@@ -13,16 +13,19 @@ DefDir=$(pwd)
 echo -e "${BO}=====================[${RED}Installing dependencies...${BO}]=====================${NC}"
 sudo pacman -S dotnet-sdk git
 echo -e "${BO}===================[${RED}Downloading AUR dependencies...${BO}]==================${NC}"
-cd /home/"$userName"/Downloads/
-git clone https://AUR.archlinux.org/visual-studio-code-bin.git
-cd visual-studio-code-bin/
-makepkg
-sudo pacman -U visual-studio-code-bin-*
-cd ..
+#cd "/home/"$userName"/Downloads/"
+#git clone https://AUR.archlinux.org/visual-studio-code-bin.git
+#cd visual-studio-code-bin/
+#makepkg
+#sudo pacman -U visual-studio-code-bin-*
+#cd ..
 echo -e "${BO}====================[${RED}Deleting AUR dependencies...${BO}]====================${NC}"
-rm -rf visual-studio-code-bin/
+#rm -rf visual-studio-code-bin/
 echo -e "${BO}======================[${RED}Copying Settings File...${BO}]======================${NC}"
+cd "/home/"$userName""
+mkdir -p .config/Code/User
 cd "$DefDir"
-cp settings.json /home/"$userName"/.config/Code/User/
+cp settings.json "/home/"$userName"/.config/Code/User/"
 echo -e "${LG}All Complete!"
 echo -e "${PUR}created by https://github.com/000Daniel${NC}"
+#created by https://github.com/000Daniel
